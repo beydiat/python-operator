c89bd604bb412e990283f08cf29d7a9497792571
beydi <beydibat@yahoo.fr>
2022-12-20 21:57:54 +0100

    init commit

---
+++ operations.py
@@ -0,0 +1,5 @@
+def add(op1, op2):
+    return op1 + op2
+
+def subs(op1, op2):
+    return op1 - op2
---
+++ test.py
@@ -0,0 +1,5 @@
+from operations import add, subs
+
+assert add(1, 2) == 3
+
+assert subs(2, 1) == 1
