1e4c0be06ebd5cf8e3ca09e30d534e213ab5f4fc
beydi <beydibat@yahoo.fr>
2023-02-23 20:21:14 +0100

    ajout gestion de log

--- ias/main.tf
+++ ias/main.tf
@@ -50,32 +50,32 @@ data "archive_file" "zip" {
 }
 
 #CloudwatchLogs
-# resource "aws_cloudwatch_log_group" "function_log_group" {
-#   name              = "/aws/lambda/${aws_lambda_function.myfunc.function_name}"
-#   retention_in_days = 7
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
+resource "aws_cloudwatch_log_group" "function_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.myfunc.function_name}"
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
+}
 
-# resource "aws_iam_policy" "function_logging_policy" {
-#   name   = "function-logging-policy"
-#   policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         Action : [
-#           "logs:CreateLogStream",
-#           "logs:PutLogEvents"
-#         ],
-#         Effect : "Allow",
-#         Resource : "arn:aws:logs:*:*:*"
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_policy" "function_logging_policy" {
+  name   = "function-logging-policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
-#   role = aws_iam_role.iam_for_lambda.id
-#   policy_arn = aws_iam_policy.function_logging_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
+  role = aws_iam_role.iam_for_lambda.id
+  policy_arn = aws_iam_policy.function_logging_policy.arn
+}
